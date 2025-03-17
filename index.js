e13d8a092e06d0af32c16c22c6690ae77e5ea4a0
Soumya Sankar Das <soumyadas429@gmail.com>
2025-03-17 21:30:14 +0530

    learned basic get request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require("express");
+import { default as axios } from "axios";
+import express from "express";
 const app = express();
 const port = require("dotenv").config().parsed.PORT;
 
@@ -7,13 +8,37 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-    res.send('about me')
+    res.send("about me");
 });
 
 app.get("/contact", (req, res) => {
     res.send("<h1>contact me</h1>");
-})
+});
+
+app.get("/github", (req, res) => {
+    res.redirect("https://github.com/Soumya-0x000");
+});
+
+app.get("/gitdetails", async (req, res) => {
+    try {
+        const gitDetailsRes = await axios.get(
+            "https://api.github.com/users/Soumya-0x000"
+        );
+        res.json(gitDetailsRes.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+app.get("/gitdetail", (req, res) => {
+    res.redirect("https://api.github.com/users/Soumya-0x000");
+});
+
+app.get("/portfolio", (req, res) => {
+    res.redirect("https://ssd-portfolio.vercel.app/");
+});
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
-})
+});
