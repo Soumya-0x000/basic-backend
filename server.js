44f8f49364e3abe971ad10dc1cc67db6107c4054
Soumya Sankar Das <soumyadas429@gmail.com>
2026-03-26 22:13:06 +0530

    growing

---
+++ server.js
@@ -0,0 +1,9 @@
+import { configDotenv } from 'dotenv';
+import { app } from './src/app.js';
+
+configDotenv();
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port} 💻💻`);
+});
